748fd03f2eb66b05514ff98e244e2ba4da1ad1b5
riskaadisti <riskaariski@gmail.com>
2019-02-01 11:47:53 +0700

    video eps 3

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,7 @@
+ <ul class="sidebar navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('admin.home')}}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+          </a>
+  </ul>
